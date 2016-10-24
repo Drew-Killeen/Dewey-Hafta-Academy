a43514714994b48d7f4916f29ef0a73dbb5a1607
Drew-Killeen <drewkilleen00@gmail.com>
2016-10-24 19:18:32 -0400

    added ability to view current exam questions

--- admin/edit_exam.php
+++ admin/edit_exam.php
@@ -25,26 +25,52 @@
     ?>
         <!--Authorized-->
         <h1>Administrator Control Panel</h1>
-        <h2>Current Exams</h2>
-        <?php
-            $courses = mysql_query("SELECT course FROM courses");
-            if (mysql_num_rows($courses) > 0) 
+            <?php
+            if(!$_GET['exam']) {
+                echo "<h2>Current Exams</h2>";
+                $courses = mysql_query("SELECT course FROM courses");
+                if (mysql_num_rows($courses) > 0) 
+                {
+                    // output data of each row
+                    while($rowCourse = mysql_fetch_assoc($courses)) {
+                        echo "<h4>".$rowCourse['course']."</h4><ul>";
+                        $exams = mysql_query("SELECT id,module,title FROM exams WHERE course='".$rowCourse['course']."' ORDER BY module ASC");
+                        while($rowExam = mysql_fetch_assoc($exams)) {
+                            echo "<li><a href='?exam=".$rowExam['id']."'>Module ".$rowExam['module'].": ".$rowExam['title']."</a></li>";
+                        }
+                        echo "</ul>";
+
+                }
+                } else 
+                {
+                    echo "0 results";
+                }
+            } else
             {
-                // output data of each row
-                while($rowCourse = mysql_fetch_assoc($courses)) {
-                    echo "<h4>".$rowCourse['course']."</h4><ul>";
-                    $exams = mysql_query("SELECT module,title FROM exams WHERE course='".$rowCourse['course']."'");
-                    while($rowExam = mysql_fetch_assoc($exams)) {
-                        echo "<li>Module ".$rowExam['module'].": ".$rowExam['title']."</li>";
+                $examName = mysql_fetch_assoc(mysql_query("SELECT module,title FROM exams WHERE id='".$_GET['exam']."'"));
+                echo "<h2>Module ".$examName['module'].": ".$examName['title']."</h2>";
+                $examQuestions = mysql_query("SELECT * FROM questions WHERE examNum='".$_GET['exam']."'");
+                if (mysql_num_rows($examQuestions) > 0) 
+                {
+                    // output data of each row
+                    while($row = mysql_fetch_assoc($examQuestions)) {
+                        echo 
+                            "<form action='' method='post'>
+                            <input type='field' name='question' value='".$row['question']."'/><br>
+                            <input type='field' name='option1' value='".$row['option1']."'/><br>
+                            <input type='field' name='option2' value='".$row['option2']."'/><br>
+                            <input type='field' name='option3' value='".$row['option3']."'/><br>
+                            <input type='field' name='option4' value='".$row['option4']."'/><br>
+                            <input type='field' name='option5' value='".$row['option5']."'/><br>
+                            <input type='submit' name='update' value='update' />
+                            </form><br>";
                     }
-                    echo "</ul>";
-                    
+                } else 
+                {
+                    echo "0 results";
+                }
             }
-            } else 
-            {
-                echo "0 results";
-            }
-        ?>
+            ?>
         
     <?php
 	   endif;
