d7e331d9eed3f00ef17e03a153b60d2b246ad525
Drew-Killeen <drewkilleen00@gmail.com>
2016-10-26 14:27:20 -0400

    added a fancy new menu

--- templates/menu.php
+++ templates/menu.php
@@ -1,15 +1,17 @@
-<header> <span class="toggle-button">
-        	<div class="menu-bar menu-bar-top"></div>
-        	<div class="menu-bar menu-bar-middle"></div>
-        	<div class="menu-bar menu-bar-bottom"></div>
+<?php if($_SESSION['id']) {
+    echo 
+    '<header> 
+        <span class="toggle-button">
+            <div class="menu-bar menu-bar-top"></div>
+            <div class="menu-bar menu-bar-middle"></div>
+            <div class="menu-bar menu-bar-bottom"></div>
         </span>
     <div class="menu-wrap">
         <div class="menu-sidebar">
             <ul class="menu">
                 <li><a href="main">Home</a></li>
-                <li><a href="registered">Preferences</a></li>
                 <li><a href="enroll">Enroll</a></li>
-                <?php
+                <li><a href="course">View course</a></li>';
                 if($_SESSION['privilege'] == 'admin' || $_SESSION['privilege'] == 'sysop') {
                 echo '<li class="menu-item-has-children"><a href="admin/index">Admin</a> <span class="sidebar-menu-arrow"></span>
                     <ul class="sub-menu">
@@ -20,8 +22,10 @@
                     </ul>
                 </li>';
                 }
-                ?>
-            </ul>
+                echo 
+                '</ul>
+            </div>
         </div>
-    </div>
-</header>
+    </header>';
+}
+?>
