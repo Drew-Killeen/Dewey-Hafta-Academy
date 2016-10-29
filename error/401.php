69af3c7dfa25505c7587488abec40623b5384a88
Drew-Killeen <drewkilleen00@gmail.com>
2016-10-29 15:44:21 -0400

    Create error pages

---
+++ error/401.php
@@ -0,0 +1,27 @@
+<?php require '../templates/header.php'; ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<?php include_once("../templates/htmlHeader.php") ?>
+</head>
+<body>
+    
+<div id="header">
+    <a href="../main" id="title">Dewey Hafta Academy</a>
+    <?php include_once("../templates/login.php"); ?>
+</div>
+    
+<?php include_once("../templates/subfolder_menu.php"); ?>
+    
+<div id="main">
+    <div class="container">
+        <h2>401 Error</h2>
+        <p>You are not authorized to access this page.</p>
+    </div>
+</div>
+
+
+</body>
+</html>
