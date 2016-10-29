593d7f693f0f08f5a9bb287fcb9c910496df1bf0
Drew-Killeen <drewkilleen00@gmail.com>
2016-10-29 18:46:10 -0400

    display number of questions next to exam name; also escaped user input

--- admin/edit_exam.php
+++ admin/edit_exam.php
@@ -12,7 +12,13 @@ if($_POST['add']=='New')
 
 if($_POST['update']=='Update')
 {   
-    mysql_query("UPDATE questions SET question='".$_POST['question']."', option1='".$_POST['option1']."', option2='".$_POST['option2']."', option3='".$_POST['option3']."', option4='".$_POST['option4']."', option5='".$_POST['option5']."', public=".$_POST['public']." WHERE id='".$_GET['question']."'");
+    $_POST['question'] = mysql_real_escape_string($_POST['question']);
+    $_POST['option1'] = mysql_real_escape_string($_POST['option1']);
+    $_POST['option2'] = mysql_real_escape_string($_POST['option2']);
+    $_POST['option3'] = mysql_real_escape_string($_POST['option3']);
+    $_POST['option4'] = mysql_real_escape_string($_POST['option4']);
+    $_POST['option5'] = mysql_real_escape_string($_POST['option5']);
+    mysql_query("UPDATE questions SET question='".$_POST['option1']."', option1='".$_POST['option1']."', option2='".$_POST['option2']."', option3='".$_POST['option3']."', option4='".$_POST['option4']."', option5='".$_POST['option5']."', public=".$_POST['public']." WHERE id='".$_GET['question']."'");
     header("Location: edit_exam?exam=".$_GET['exam']);
 }
 
@@ -62,7 +68,10 @@ if($_POST['updateExam']=='Update')
                         echo "<h4>".$rowCourse['course']."</h4><ul>";
                         $exams = mysql_query("SELECT id,module,title FROM exams WHERE course='".$rowCourse['course']."' ORDER BY module ASC");
                         while($rowExam = mysql_fetch_assoc($exams)) {
-                            echo "<li><a href='?exam=".$rowExam['id']."'>Module ".$rowExam['module'].": ".$rowExam['title']."</a></li>";
+                            $questionsNum = mysql_query("SELECT id FROM questions WHERE examNum=".$rowExam['id']);
+                            echo "<li><a href='?exam=".$rowExam['id']."'>Module ".$rowExam['module'].": ".$rowExam['title']."</a>";
+                            if(mysql_num_rows($questionsNum) > 0) echo "<i> - ".mysql_num_rows($questionsNum)." questions</i>";
+                            echo "</li>";
                         }
                         echo "</ul>";
 
@@ -74,28 +83,30 @@ if($_POST['updateExam']=='Update')
             } else if(!$_GET['question'])
             {
                 $examName = mysql_fetch_assoc(mysql_query("SELECT module,title,public FROM exams WHERE id='".$_GET['exam']."'"));
-                echo "<h2><a href='edit_exam'>&larr;</a> Module ".$examName['module'].": ".$examName['title']."</h2>";
-                $examQuestions = mysql_query("SELECT id,question FROM questions WHERE examNum='".$_GET['exam']."'");
+                echo "<h2><a href='edit_exam' class='black'>&larr;</a> Module ".$examName['module'].": ".$examName['title']."</h2>";
+                echo "<form action='' method='post'><b>Public</b> <input type='radio' name='public' value='1' ";
+                if($examName['public'] == 1) {echo "checked ";} 
+                echo "><label>Yes</label></input> <input type='radio' name='public' value='0' ";
+                if($examName['public'] == 0) {echo "checked ";} 
+                echo "><label>No</label></input><br><input type='submit' name='updateExam' value='Update' /><h3>Questions</h3>";
+                $examQuestions = mysql_query("SELECT id,question,public FROM questions WHERE examNum='".$_GET['exam']."'");
                 if (mysql_num_rows($examQuestions) > 0) 
                 {
                     // output data of each row
                     while($row = mysql_fetch_assoc($examQuestions)) {
-                        echo 
-                            "<b>Question:</b> ".$row['question']." (<a href='?exam=".$_GET['exam']."&question=".$row['id']."'>edit</a>)<br>";
+                        echo "<b>Question:</b> ".$row['question']." (<a href='?exam=".$_GET['exam']."&question=".$row['id']."'>edit</a>)";
+                        if($row['public'] == 0) echo " <i>... not public</i>";
+                        echo "<br>";
                     }
                 } else 
                 {
                     echo "0 results";
                 }
-                echo "<form action='' method='post'><b>Public?</b> <input type='radio' name='public' value='1' ";
-                if($examName['public'] == 1) {echo "checked ";} 
-                echo "><label>Yes</label></input> <input type='radio' name='public' value='0' ";
-                if($examName['public'] == 0) {echo "checked ";} 
-                echo "><label>No</label></input><br><input type='submit' name='updateExam' value='Update' /><input type='submit' name='add' value='New' /></form>";
+                echo "<br><input type='submit' name='add' value='New' /></form>";
             } else
             {
                 $examName = mysql_fetch_assoc(mysql_query("SELECT module,title FROM exams WHERE id='".$_GET['exam']."'"));
-                echo "<h2><a href='edit_exam?exam=".$_GET['exam']."'>&larr;</a> Module ".$examName['module'].": ".$examName['title']."</h2>";
+                echo "<h2><a href='edit_exam?exam=".$_GET['exam']."' class='black'>&larr;</a> Module ".$examName['module'].": ".$examName['title']."</h2>";
                 $examQuestions = mysql_fetch_assoc(mysql_query("SELECT * FROM questions WHERE id='".$_GET['question']."'"));
 
                 echo 
