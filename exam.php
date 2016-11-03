fcc5eb337bae12b41287dde21f619d377cd5429b
Drew-Killeen <drewkilleen00@gmail.com>
2016-11-03 14:48:19 -0400

    begin new review exam feature; also allow users to resume previous exam

--- exam.php
+++ exam.php
@@ -5,20 +5,21 @@ require 'templates/header.php';
 if(!$_GET['exam']) {
     header("Location: course");
 }
+else if($_SESSION['numQuestions']) {
+    if($_GET['question'] >= $_SESSION['numQuestions']) {
+        header("Location: exam?exam=".$_GET['exam']."&review=1");
+    }
+}
 
 $course =  mysql_fetch_assoc(mysql_query("SELECT enrollment FROM dewey_members WHERE id='{$_SESSION['id']}'"));
-$exam = $_GET['exam'];
-$temp = mysql_fetch_assoc(mysql_query("SELECT id,title,module FROM exams WHERE course='".$course['enrollment']."' AND id='".$exam."'"));
-$examNum = $temp["id"];
+$temp = mysql_fetch_assoc(mysql_query("SELECT title,module FROM exams WHERE course='".$course['enrollment']."' AND id='".$_GET['exam']."'"));
 $examModule = $temp['module'];
 $examTitle = $temp["title"];
 
-$attempt = mysql_fetch_assoc(mysql_query("SELECT * FROM attempts WHERE usr='".$_SESSION['usr']."' AND examNum='".$exam."' ORDER BY id DESC LIMIT 1;"));
+$attempt = mysql_fetch_assoc(mysql_query("SELECT * FROM attempts WHERE usr='".$_SESSION['usr']."' AND examNum='".$_GET['exam']."' ORDER BY id DESC LIMIT 1;"));
     
 if($_GET['question']) $questionNum = $_GET['question']+1;
 else $questionNum = 1;
-
-if(!$_SESSION['examData']) $_SESSION['examData'] = mysql_query ("SELECT * FROM questions WHERE examNum='".$examNum."' AND public=1 ORDER BY RAND()");
 
 if($_POST['continue'] == 'Continue') {
     /* When continue button is pressed */
@@ -28,18 +29,20 @@ if($_POST['continue'] == 'Continue') {
     else $correct = 0;
     $attemptNum = $attempt['id'] + 1;
     mysql_query("UPDATE answers SET correct=".$correct." WHERE questionNum=".$questionCorrect['id']." AND attemptNum=".$attempt['id']);
-    header("Location: exam?exam=".$exam."&question=".$questionNum);
+    if($_GET['question'] == $_SESSION['numQuestions']-1) header("Location: exam?exam=".$_GET['exam']."&review=1");
+    else header("Location: exam?exam=".$_GET['exam']."&question=".$questionNum);
 }
 
 if($_POST['begin'] == 'Begin') {
     /* When exam begins */
     mysql_query("INSERT INTO attempts (examNum,usr,score) VALUES (".$_GET['exam'].", '".$_SESSION['usr']."', -1)");
     $_SESSION['attemptNum'] = mysql_fetch_assoc(mysql_query("SELECT id FROM attempts WHERE usr='".$_SESSION['usr']."' AND examNum=".$_GET['exam']." ORDER BY id DESC LIMIT 1;"));
+    $examData = mysql_query("SELECT * FROM questions WHERE examNum='".$_GET['exam']."' AND public=1 ORDER BY RAND()");
     $i = 1;
     // output data of each row
-    while($row = mysql_fetch_assoc($_SESSION['examData'])) {
+    while($row = mysql_fetch_assoc($examData)) {
         $attemptNum = $attempt['id'] + 1;
-        mysql_query("INSERT INTO answers (questionNum, attemptNum, correct, usr) VALUES (".$row['id'].", ".$attemptNum.", 2, '".$_SESSION['usr']."')");
+        mysql_query("INSERT INTO answers (examNum, questionNum, attemptNum, correct, usr) VALUES (".$_GET['exam'].", ".$row['id'].", ".$attemptNum.", 2, '".$_SESSION['usr']."')");
         $_SESSION['options'][$i] = array();
         $_SESSION['questions'][$i] = $row['question'];
         if($row['option1']) array_push($_SESSION['options'][$i], $row['option1']);
@@ -50,7 +53,53 @@ if($_POST['begin'] == 'Begin') {
         shuffle($_SESSION['options'][$i]);
         $i++;
     }
-    header("Location: exam?exam=".$exam."&question=1");
+    $_SESSION['numQuestions'] = $i;
+    header("Location: exam?exam=".$_GET['exam']."&question=1");
+}
+
+if($_POST['continue_attempt'] == "Continue") {
+    
+    $currentAttempt = mysql_fetch_assoc(mysql_query("SELECT id FROM attempts WHERE usr='".$_SESSION['usr']."' AND examNum=".$_GET['exam']." ORDER BY id DESC LIMIT 1;"));
+    if($_SESSION['attemptNum'] != $currentAttempt) {
+        $_SESSION['attemptNum'] = $currentAttempt;
+        $examData = mysql_query("SELECT * FROM questions WHERE examNum='".$_GET['exam']."' AND public=1 ORDER BY RAND()");
+        $i = 1;
+        // output data of each row
+        while($row = mysql_fetch_assoc($examData)) {
+            $attemptNum = $attempt['id'] + 1;
+            mysql_query("INSERT INTO answers (examNum, questionNum, attemptNum, correct, usr) VALUES (".$_GET['exam'].", ".$row['id'].", ".$attemptNum.", 2, '".$_SESSION['usr']."')");
+            $_SESSION['options'][$i] = array();
+            $_SESSION['questions'][$i] = $row['question'];
+            if($row['option1']) array_push($_SESSION['options'][$i], $row['option1']);
+            if($row['option2']) array_push($_SESSION['options'][$i], $row['option2']);
+            if($row['option3']) array_push($_SESSION['options'][$i], $row['option3']);
+            if($row['option4']) array_push($_SESSION['options'][$i], $row['option4']);
+            if($row['option5']) array_push($_SESSION['options'][$i], $row['option5']);
+            shuffle($_SESSION['options'][$i]);
+            $i++;
+        }
+        $_SESSION['numQuestions'] = $i;
+    }
+    header("Location: exam?exam=".$_GET['exam']."&question=1");
+
+    /*$_SESSION['attemptNum'] = mysql_fetch_assoc(mysql_query("SELECT id FROM attempts WHERE usr='".$_SESSION['usr']."' AND examNum=".$_GET['exam']." ORDER BY id DESC LIMIT 1;"));
+    $prevAnswerData = mysql_query("SELECT questionNum FROM answers WHERE examNum='".$_GET['exam']."' AND usr=".$_SESSION['usr']);
+    // output data of each row
+    for($i = 1; $i < mysql_num_rows($prevAnswerData); $i++) {
+        $row = mysql_fetch_assoc($prevAnswerData);
+        $row = mysql_fetch_assoc(mysql_query("SELECT * FROM questions WHERE id=".$row['questionNum']));
+        $_SESSION['options'][$i] = array();
+        $_SESSION['questions'][$i] = $row['question'];
+        if($row['option1']) array_push($_SESSION['options'][$i], $row['option1']);
+        if($row['option2']) array_push($_SESSION['options'][$i], $row['option2']);
+        if($row['option3']) array_push($_SESSION['options'][$i], $row['option3']);
+        if($row['option4']) array_push($_SESSION['options'][$i], $row['option4']);
+        if($row['option5']) array_push($_SESSION['options'][$i], $row['option5']);
+        shuffle($_SESSION['options'][$i]);
+    }
+    
+    $_SESSION['numQuestions'] = $i;
+    header("Location: exam?exam=".$_GET['exam']."&question=1");*/
 }
 
 if($_POST['submit'] == 'Submit') {
@@ -61,12 +110,11 @@ if($_POST['submit'] == 'Submit') {
     {
         if($row['correct'] == 1) $finalScore++;
     }
-    $finalScore = ($finalScore/mysql_num_rows($answersData)) * 100;
-    mysql_query("UPDATE attempts SET score=".$finalScore." WHERE usr='".$_SESSION['usr']."' AND examNum='".$exam."' AND score=-1");
+    $finalScore = round(($finalScore/mysql_num_rows($answersData)) * 100);
+    mysql_query("UPDATE attempts SET score=".$finalScore." WHERE usr='".$_SESSION['usr']."' AND examNum='".$_GET['exam']."' AND score=-1");
     unset($_SESSION['questions']);
     unset($_SESSION['question']);
     unset($_SESSION['options']);
-    unset($_SESSION['examData']);
     header("Location: grade?attempt=".$attempt['id']);
 }
 ?>
@@ -138,10 +186,16 @@ if($_POST['submit'] == 'Submit') {
                     } 
                     echo "/><label for='".$_SESSION['options'][$_GET['question']][4]."'>".$_SESSION['options'][$_GET['question']][4]."</label><br>";
                 }
-            } else if($attempt['score'] == -1) 
+            } 
+            else if($_GET['review'] == 1) 
             {
-                echo "<p>Your previous attempt on this exam is incomplete. Click below to continue your current attempt.</p> <br> <input type='submit' name='continue' value='Continue' />";
-            } else 
+                echo "<p>This is a review.</p>";
+            } 
+            else if($attempt['score'] == -1) 
+            {
+                echo "<p>Your previous attempt on this exam is incomplete. Click below to continue your current attempt.</p> <br> <input type='submit' name='continue_attempt' value='Continue' />";
+            } 
+            else 
             {
                 echo "<p>You are about to begin an exam. You have 0 attempts remaining. If there is a time limit, the countdown will begin as soon as your press continue. Blah, blah, blah.</p> <br> <input type='submit' name='begin' value='Begin' />";
             }
