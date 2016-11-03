4649625e867867c0a80a7142949987b17fdad78b
Drew-Killeen <drewkilleen00@gmail.com>
2016-11-03 16:31:50 -0400

    added new functionality
    Grade.php now displays a list of attempts and whether you got each
    question correct on a specific attempt.

--- grade.php
+++ grade.php
@@ -1,6 +1,6 @@
-<?php 
-require 'templates/header.php'; 
+<?php
 
+require 'templates/header.php'; 
 
 ?>
 
@@ -32,7 +32,43 @@ require 'templates/header.php';
         else:
     ?>
         <!--Authorized-->
-        
+        <?php
+        if(!$_GET['attempt']) {
+            $course =  mysql_fetch_assoc(mysql_query("SELECT enrollment FROM dewey_members WHERE id='{$_SESSION['id']}'"));
+            $exams = mysql_query("SELECT id,module,title FROM exams WHERE course='".$course['enrollment']."' ORDER BY  `exams`.`module` ASC ");
+            if (mysql_num_rows($exams) > 0) {
+                while($row1 = mysql_fetch_assoc($exams)) {
+                    echo "<h3>Module ".$row1['module'].": ".$row1['title']."</h3>";
+                    $attempts = mysql_query("SELECT id,score FROM attempts WHERE usr=".$_SESSION['id']." AND examNum=".$row1['id']);
+                    if (mysql_num_rows($attempts) > 0) {
+                        echo "<ul>";
+                        for($i = 1; $row2 = mysql_fetch_assoc($attempts); $i++) {
+                            echo "<li>Attempt ".$i.": ".$row2['score']."% (<a href='grade?attempt=".$row2['id']."'>review</a>)</li>";
+                        }
+                        echo "</ul>";
+                    }
+                    else {
+                        echo "Not attempts yet.";
+                    }
+                }
+            }
+            else {
+                echo "0 results";
+            }
+        }
+        else {
+            $answers = mysql_query("SELECT * FROM answers WHERE attemptNum=".$_GET['attempt']);
+            $score = mysql_fetch_assoc(mysql_query ("SELECT score FROM attempts WHERE id=".$_GET['attempt']));
+            echo "<h4>Score: ".$score['score']."%</h4>";
+            echo "<ul>";
+            for($i = 1; $row = mysql_fetch_assoc($answers); $i++) {
+                if($row['correct'] == 1) $correct = "<span class='green'>correct</span>";
+                else $correct = "<span class='red'>incorrect</span>";
+                echo "<li>Question ".$i.": ".$correct."</li>";
+            }
+            echo "</ul>";
+        }
+        ?>
     <?php
 	   endif;
     ?>
