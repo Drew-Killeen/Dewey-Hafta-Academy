0a40439e5879a17dd1488a7d8faca5fe4ef90308
Drew-Killeen <drewkilleen00@gmail.com>
2016-11-18 12:46:31 -0500

    new superpower: deleting stuff

---
+++ scripts/delete.php
@@ -0,0 +1,27 @@
+<?php
+    require '../templates/header.php';
+
+    if(($_GET['question'] && $_GET['exam']) || ($_GET['question'] && $_GET['course']) || ($_GET['exam'] && $_GET['course']))
+    {
+        $_SESSION['msg']['err'] = "Error: You can only delete one thing at a time.";
+    }
+    else if($_GET['question'] && ($_SESSION['privilege'] == "administrator" || $_SESSION['privilege'] == "sysop")) 
+    {
+        mysql_query("DELETE FROM questions WHERE id=".$_GET['question']);
+        $_SESSION['msg']['success']='Question successfully deleted.';
+    }
+    else if($_GET['exam'] && ($_SESSION['privilege'] == "administrator" || $_SESSION['privilege'] == "sysop")) 
+    {
+        mysql_query("DELETE FROM exams WHERE id=".$_GET['exam']);
+        $_SESSION['msg']['success']='Exam successfully deleted.';
+    }
+    else if($_GET['course'] && ($_SESSION['privilege'] == "administrator" || $_SESSION['privilege'] == "sysop")) 
+    {
+        mysql_query("DELETE FROM courses WHERE id=".$_GET['course']);
+        $_SESSION['msg']['success']='Course successfully deleted.';
+    }
+    else 
+    {
+        $_SESSION['msg']['err'] = "Error: Deletion failed.";
+    }
+?>
