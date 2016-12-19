e88554feec180401bfcf0ea49e336c2c1650d68c
Drew-Killeen <drewkilleen00@gmail.com>
2016-12-19 14:01:16 -0500

    updated usr header

--- registered.php
+++ registered.php
@@ -150,9 +150,21 @@ if($_POST['request']=='Send Request')
     
 <div id="header">
     <a href="main" id="title">Dewey Hafta Academy</a>
-    <?php if($_SESSION['id']): ?>
-    <span id="usrHeader"><a href="?logoff">Logout</a></span>
+    <span id="usrHeader">
+    <?php if(!$_SESSION['id']): ?>
+    <a href="../sign_in">Login</a> - <a href="../sign_up">Sign up</a>
+    <?php else: ?>
+        <div class="menu-dropdown">
+            <ul class="menu nolist">
+                <li class="menu-item-has-children" style="text-align:right;"><a href="../profile"><?php echo $_SESSION['usr']; ?></a> <span class="sidebar-menu-arrow"></span>
+                    <ul class="sub-menu-dropdown nolist" style="display:none;">
+                        <li><a href="?logoff">Logout</a></li>
+                    </ul>
+                </li>
+                </ul>
+            </div>
     <?php endif; ?>
+</span>
     
 </div>
     
