d91b928533d6d2bf1ebf53c460e72112d7638748
Drew-Killeen <drewkilleen00@gmail.com>
2016-12-19 13:43:29 -0500

    merging subfolder_menu.php into menu.php

--- error/404.php
+++ error/404.php
@@ -13,7 +13,7 @@
     <?php include_once("../templates/login.php"); ?>
 </div>
     
-<?php include_once("../templates/subfolder_menu.php"); ?>
+<?php include_once("../templates/menu.php"); ?>
     
 <div id="main">
     <div class="container">
