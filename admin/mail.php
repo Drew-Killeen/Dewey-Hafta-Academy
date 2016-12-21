79affe9de5f5c7607f85efc30ae1705efe5b438f
Drew-Killeen <drewkilleen00@gmail.com>
2016-12-21 18:47:46 -0500

    creating placeholder

---
+++ admin/mail.php
@@ -0,0 +1,41 @@
+<?php require '../scripts/header.php'; ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<?php include_once("../templates/htmlHeader.php") ?>
+</head>
+<body>
+    
+<div id="header">
+    <a href="../main" id="title">Dewey Hafta Academy</a>
+    <?php include_once("../templates/login.php"); ?>
+</div>
+    
+<?php include_once("../templates/menu.php"); ?>
+    
+<div id="main">
+    <div class="container">
+    <?php
+        if(!$_SESSION['id'] || ($_SESSION['privilege'] != admin && $_SESSION['privilege'] != sysop)):
+    ?>
+        <!--Unauthorized-->
+        <p>You do not have sufficient privileges to access this page.</p>
+    <?php
+        else:
+    ?>
+        <!--Authorized-->
+        <h1>Administrator Control Panel</h1>
+        <p>--placeholder for mail features--</p>
+        
+    <?php
+	   endif;
+    ?>
+    </div>
+</div>
+    
+<?php require '../scripts/jsload.php'; ?>
+
+</body>
+</html>
