d33526e78dbf562a76d8f9e91c3a4bcd403889e7
Drew-Killeen <drewkilleen00@gmail.com>
2016-12-21 13:25:39 -0500

    503 error page

---
+++ error/503.php
@@ -0,0 +1,27 @@
+<?php require '../scripts/header.php'; ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<?php include_once("../templates/htmlHeader.php") ?>
+</head>
+<body>
+    
+<div id="header">
+    <a href="../main" id="title">Dewey Hafta Academy</a>
+    <?php include_once("../templates/login.php"); ?>
+</div>
+    
+<?php include_once("../templates/menu.php"); ?>
+    
+<div id="main">
+    <div class="container">
+        <h2>503 Error</h2>
+        <p>Service unavailable.</p>
+    </div>
+</div>
+
+
+</body>
+</html>
