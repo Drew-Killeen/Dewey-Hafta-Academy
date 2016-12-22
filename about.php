86efc4d68d4ae1db681d757a643e621ca44796b8
Drew-Killeen <drewkilleen00@gmail.com>
2016-12-22 14:57:53 -0500

    integrate rich text editor

--- about.php
+++ about.php
@@ -5,28 +5,39 @@
     if(!$_GET['article'] && $_GET['action'] != 'new') {
         header('Location: ../about?article=1');
     }
+    if($_POST['submit'] == 'Submit') {
+        mysql_query("UPDATE help SET content='".$_POST['textarea']."' WHERE id=".$_GET['article']);
+        header('Location: ?article='.$_GET['article']);
+        exit();
+    }
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
 
-<?php include_once("templates/htmlHeader.php") ?>
-
+<?php if($_GET['action'] != "edit") include_once("templates/htmlHeader.php");
+    else echo "<script src='//cdn.tinymce.com/4/tinymce.min.js'></script>";
+?>
+    
+<script>
+  tinymce.init({
+    selector: '#textarea'
+  });
+</script>
+    
 </head>
 <body>
     
-<div id="header">
-    <a href="../main" id="title">Dewey Hafta Academy</a>
-    
-    <?php include_once("templates/login.php"); ?>
-</div>
-    
-<?php include_once("templates/menu.php"); ?>
+    <?php if($_GET['action'] != "edit"): ?>
+<?php 
+    include_once("templates/login.php"); 
+    include_once("templates/menu.php"); 
+?>
     
 <div id="main">
     <div class="container">
-    <?php
+    <?php endif;
         if(!$_SESSION['id']):
     ?>
         <!--Unauthorized-->
@@ -36,23 +47,24 @@
     ?>
         <!--Authorized-->
     <?php
-        if($_SESSION['privilege'] == 'administrator' || $_SESSION['privilege'] == 'sysop') {
+        if(($_SESSION['privilege'] == 'administrator' || $_SESSION['privilege'] == 'sysop') && $_GET['action'] != "edit") {
             echo "<div class='helpEdit'><input type='button' value='Edit' onclick='window.location.assign(\"../about?article=".$_GET['article']."&action=edit\")'/> <input type='button' value='New' onclick='window.location.assign(\"../about?action=new\")'/></div>";
         }
-        if($_GET['action'] && ($_SESSION['privilege'] != 'administrator' || $_SESSION['privilege'] != 'sysop')) {
+        /*if($_GET['action'] && ($_SESSION['privilege'] != 'administrator' || $_SESSION['privilege'] != 'sysop')) {
             $_SESSION['msg']['err'] = "You do not have sufficient privileges to perform this action.";
             header('Location: ../about?article=1');
             exit();
         }
-        else if($_GET['action'] == 'new') {
+        else*/ if($_GET['action'] == 'new') {
             
         }
         else if($_GET['action'] == 'edit') {
-            
+            $content = mysql_fetch_assoc(mysql_query("SELECT title,content FROM help WHERE id=".$_GET['article']));
+            echo "<form method='post'><textarea id='textarea' name='textarea'><p>".$content['content']."</p></textarea><input type='submit' name='submit' value='Submit'/></form>";
         }
         else if($_GET['article'] == 1) {
             $content = mysql_fetch_assoc(mysql_query("SELECT title,content FROM help WHERE id=1"));
-            echo "<h2>".$content['title']."</h2><p>".$content['content']."</p>";
+            echo "<p>".$content['content']."</p>";
         
             $articles = mysql_query("SELECT id,title FROM help WHERE public=1");
             echo "<ul>";
@@ -63,17 +75,18 @@
         }
         else {
             $content = mysql_fetch_assoc(mysql_query("SELECT title,content FROM help WHERE id=".$_GET['article']));
-            echo "<h2>".$content['title']."</h2><p>".$content['content']."</p>";
+            echo "<p>".$content['content']."</p>";
         }
     ?>
         
     <?php
-	   endif;
+        endif;
+        if($_GET['action'] != "edit"):
     ?>
     </div>
 </div>
     
-<?php require 'scripts/jsload.php'; ?>
+<?php endif; require 'scripts/jsload.php'; ?>
 
 </body>
 </html>
