5f389e9482733fe1b907012339d3b02d9ba83599
Drew-Killeen <drewkilleen00@gmail.com>
2017-01-03 11:36:20 -0500

    updates to editing help pages

--- about.php
+++ about.php
@@ -6,7 +6,7 @@
         header('Location: ../about?article=1');
     }
     if($_POST['submit'] == 'Submit') {
-        mysql_query("UPDATE help SET content='".$_POST['textarea']."' WHERE id=".$_GET['article']);
+        mysql_query("UPDATE help SET content='".$_POST['textarea']."', public=".$_POST['public']." WHERE id=".$_GET['article']);
         header('Location: ?article='.$_GET['article']);
         exit();
     }
@@ -22,14 +22,18 @@
     
 <script>
   tinymce.init({
-    selector: '#textarea'
+    selector: '#textarea',
+    plugins: "code",
+    toolbar: "code",
   });
 </script>
     
 </head>
+
+<?php if($_GET['action'] != "edit"): ?>  
+
 <body>
-    
-    <?php if($_GET['action'] != "edit"): ?>
+
 <?php 
     include_once("templates/login.php"); 
     include_once("templates/menu.php"); 
@@ -37,7 +41,7 @@
     
 <div id="main">
     <div class="container">
-    <?php endif;
+    <?php
         if(!$_SESSION['id']):
     ?>
         <!--Unauthorized-->
@@ -50,19 +54,7 @@
         if(($_SESSION['privilege'] == 'administrator' || $_SESSION['privilege'] == 'sysop') && $_GET['action'] != "edit") {
             echo "<div class='helpEdit'><input type='button' value='Edit' onclick='window.location.assign(\"../about?article=".$_GET['article']."&action=edit\")'/> <input type='button' value='New' onclick='window.location.assign(\"../about?action=new\")'/></div>";
         }
-        /*if($_GET['action'] && ($_SESSION['privilege'] != 'administrator' || $_SESSION['privilege'] != 'sysop')) {
-            $_SESSION['msg']['err'] = "You do not have sufficient privileges to perform this action.";
-            header('Location: ../about?article=1');
-            exit();
-        }
-        else*/ if($_GET['action'] == 'new') {
-            
-        }
-        else if($_GET['action'] == 'edit') {
-            $content = mysql_fetch_assoc(mysql_query("SELECT title,content FROM help WHERE id=".$_GET['article']));
-            echo "<form method='post'><textarea id='textarea' name='textarea'><p>".$content['content']."</p></textarea><input type='submit' name='submit' value='Submit'/></form>";
-        }
-        else if($_GET['article'] == 1) {
+        if($_GET['article'] == 1) {
             $content = mysql_fetch_assoc(mysql_query("SELECT title,content FROM help WHERE id=1"));
             echo "<p>".$content['content']."</p>";
         
@@ -78,15 +70,55 @@
             echo "<p>".$content['content']."</p>";
         }
     ?>
-        
-    <?php
-        endif;
-        if($_GET['action'] != "edit"):
-    ?>
     </div>
 </div>
     
 <?php endif; require 'scripts/jsload.php'; ?>
 
 </body>
+    
+<?php 
+    else: 
+    include_once("templates/htmlHeader.php");
+?>
+    
+    
+<body>
+    
+<?php include_once("templates/login.php");  ?>
+    
+    <div class='editor-container'>
+    <?php
+        /*if($_GET['action'] && ($_SESSION['privilege'] != 'administrator' || $_SESSION['privilege'] != 'sysop')) {
+        $_SESSION['msg']['err'] = "You do not have sufficient privileges to perform this action.";
+        header('Location: ../about?article=1');
+        exit();
+        }
+        else*/ if($_GET['action'] == 'new') {
+
+        }
+        else if($_GET['action'] == 'edit') {
+            $content = mysql_fetch_assoc(mysql_query("SELECT title,content,public FROM help WHERE id=".$_GET['article']));
+            echo 
+                "<form method='post'>
+                    <textarea id='textarea' name='textarea'><p>".$content['content']."</p></textarea>
+                    <b>Public:</b>
+                    <input type='radio' name='public' value='1' ";
+                    if($content['public'] == 1) echo "checked";
+                    echo "/><label for='1'>Yes</label>
+                    <input type='radio' name='public' value='0' ";
+                    if($content['public'] == 0) echo "checked";
+                    echo "/><label for='0'>No</label>
+                    <input type='submit' name='submit' class='editor-submit' value='Submit'/>
+                </form>";
+        }
+    ?>
+    </div>
+</body>
+
+<?php
+    require 'scripts/jsload.php';
+    endif;
+?>
+
 </html>
