ea4d770f84f4e050e3b17de3e8d4c3bcc0177c4a
Drew-Killeen <drewkilleen00@gmail.com>
2017-01-19 19:40:17 -0500

    delete attempts

--- scripts/delete.php
+++ scripts/delete.php
@@ -1,4 +1,5 @@
 <?php
+
     require '../scripts/header.php';
 
     if(($_GET['question'] && $_GET['exam']) || ($_GET['question'] && $_GET['course']) || ($_GET['exam'] && $_GET['course'])) {
@@ -6,15 +7,15 @@
     }
     else if($_GET['question'] && ($_SESSION['privilege'] == "admin" || $_SESSION['privilege'] == "sysop")) {
         mysqli_query($link, "DELETE FROM questions WHERE id=".$_GET['question']);
-        $_SESSION['msg']['success']='Question successfully deleted.';
+        $_SESSION['msg']['success']='Question deleted.';
     }
     else if($_GET['exam'] && ($_SESSION['privilege'] == "admin" || $_SESSION['privilege'] == "sysop")) {
         mysqli_query($link, "DELETE FROM exams WHERE id=".$_GET['exam']);
-        $_SESSION['msg']['success']='Exam successfully deleted.';
+        $_SESSION['msg']['success']='Exam deleted.';
     }
     else if($_GET['course'] && ($_SESSION['privilege'] == "admin" || $_SESSION['privilege'] == "sysop")) {
         mysqli_query($link, "DELETE FROM courses WHERE id=".$_GET['course']);
-        $_SESSION['msg']['success']='Course successfully deleted.';
+        $_SESSION['msg']['success']='Course deleted.';
     }
     else if($_GET['user'] && ($_SESSION['id'] == $_GET['user'] || $_SESSION['privilege'] == "sysop" || $_SESSION['privilege'] == "admin")) {
         if($_SESSION['id'] == $_GET['user']) {
@@ -22,7 +23,12 @@
             session_destroy();
         }
         mysqli_query($link, "DELETE FROM dewey_members WHERE id=".$_GET['user']);
-        $_SESSION['msg']['success']='Account successfully deleted.';
+        $_SESSION['msg']['success']='Account deleted.';
+    }
+    else if($_GET['attempt'] && ($_SESSION['privilege'] == "teacher" || $_SESSION['privilege'] == "admin" || $_SESSION['privilege'] == "sysop")) {
+        mysqli_query($link, "DELETE FROM attempts WHERE id=".$_GET['attempt']);
+        mysqli_query($link, "DELETE FROM answers WHERE attemptNum=".$_GET['attempt']);
+        $_SESSION['msg']['success']='Attempt deleted.';
     }
     else {
         $_SESSION['msg']['err'] = "Error: Deletion failed.";
