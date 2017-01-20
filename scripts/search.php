68b7e5b47194fd7ed28725e6b473dc97659aa89b
Drew-Killeen <drewkilleen00@gmail.com>
2017-01-19 19:40:06 -0500

    only search for teachers

--- scripts/search.php
+++ scripts/search.php
@@ -3,7 +3,7 @@
 
     $key=$_GET['key'];
     $array = array();
-    $query=mysqli_query($link, "SELECT * FROM dewey_members WHERE usr LIKE '%{$key}%'");
+    $query=mysqli_query($link, "SELECT * FROM dewey_members WHERE usr LIKE '%{$key}%' AND (privilege='teacher' OR privilege='admin' OR privilege='sysop')");
     while($row=mysqli_fetch_assoc($query))
     {
       $array[] = $row['usr'];
