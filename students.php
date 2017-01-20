4ca7aaf9b6110a527484f3b9cb6cccc9acab48d5
Drew-Killeen <drewkilleen00@gmail.com>
2017-01-19 19:42:49 -0500

    formatting update; teachers can delete attempts

--- students.php
+++ students.php
@@ -1,4 +1,13 @@
-<?php require 'scripts/header.php'; ?>
+<?php 
+    require 'scripts/header.php'; 
+
+    if($_POST['submit']=='Submit') {
+        $_POST['examType'] = mysqli_real_escape_string($link, $_POST['examType']);
+        mysqli_query($link, "UPDATE dewey_members SET examType=".$_POST['examType']." WHERE id=".$_GET['id']);
+        $_SESSION['msg']['success']='Preferences updated.';
+    }
+
+?>
 
 <!DOCTYPE html>
 <html>
@@ -13,6 +22,17 @@
     include_once("templates/login.php"); 
     include_once("templates/menu.php"); 
 ?>
+<script>
+    function deleteAttempt() {
+        $confirm = confirm("Are you sure you want to delete this attempt? This cannot be undone.");
+        if($confirm === true) {
+            var xhttp = new XMLHttpRequest();
+            <?php echo 'xhttp.open("GET", "../scripts/delete.php?attempt='.$_GET['attempt'].'", true);'; ?>
+            xhttp.send();
+            window.location.assign(<?php echo '"../students?id='.$_GET['id'].'"'; ?>);
+        }
+    }
+</script>
     
 <div id="main">
     <div class="container">
@@ -40,10 +60,21 @@
             }
         }
         else if(!$_GET['attempt']) {
-            $currentStudent = mysqli_fetch_assoc(mysqli_query($link, "SELECT usr,enrollment FROM dewey_members WHERE id='{$_GET['id']}'"));                
+            $currentStudent = mysqli_fetch_assoc(mysqli_query($link, "SELECT usr,enrollment,examType FROM dewey_members WHERE id='{$_GET['id']}'"));                
             $exams = mysqli_query($link, "SELECT id,module,title FROM exams WHERE course='".$currentStudent['enrollment']."' ORDER BY  `exams`.`module` ASC ");
             
-            echo "<h2>Student progress for ".$currentStudent['usr']."</h2>";
+            echo "
+                <h1>Teacher Portal</h1>
+                <h2>".$currentStudent['usr']."</h2>
+                <form method='post' action=''>Preferred exam type:<br>
+                <p><label><input type='radio' name='examType' value='0' ";
+            if($currentStudent['examType'] == 0) echo "checked";
+            echo "/>Multiple choice</label><br><label><input type='radio' name='examType' value='1' ";
+            if($currentStudent['examType'] == 1) echo "checked";
+            echo "/>Free response</label></p>
+                <input type='submit' name='submit' value='Submit'/>
+                </form><br><br>
+                <h2>Student Progress</h2>";
             
             if (mysqli_num_rows($exams) > 0) {
                 while($row1 = mysqli_fetch_assoc($exams)) {
@@ -78,13 +109,15 @@
                 echo "You do not have permission to view this attempt.";
             }
             else {
-                $score = mysqli_fetch_assoc(mysqli_query ($link, "SELECT usr,score FROM attempts WHERE id=".$_GET['attempt']));
+                $score = mysqli_fetch_assoc(mysqli_query ($link, "SELECT examNum,usr,score FROM attempts WHERE id=".$_GET['attempt']));
                 if($_SESSION['id'] != $score['usr'] && ($_SESSION['privilege'] != 'admin' && $_SESSION['privilege'] != 'sysop')) {
                     echo "You do not have permission to view this attempt.";
                 }
                 else {
                     $answers = mysqli_query($link, "SELECT questionNum,correct,option FROM answers WHERE attemptNum=".$_GET['attempt']);
-                    echo "<h4>Score: ".$score['score']."%</h4>";
+                    $correctNum = mysqli_query($link, "SELECT id FROM answers WHERE attemptNum=".$_GET['attempt']." AND correct=1");
+                    $examInfo = mysqli_fetch_assoc(mysqli_query($link, "SELECT module,title FROM exams WHERE id=".$score['examNum']));
+                    echo "<h1>Attempt for Module ".$examInfo['module'].": ".$examInfo['title']."</h1><span class='attempt-score'><b>Score:</b> ".mysqli_num_rows($correctNum)." out of ".mysqli_num_rows($answers)." - ".$score['score']."%</span><span class='attempt-time'>--time--</span><hr>";
                     echo "<div class='answers-list'>";
                     for($i = 1; $row = mysqli_fetch_assoc($answers); $i++) {
                         $questions = mysqli_fetch_assoc(mysqli_query($link, "SELECT question,option1,option2,option3,option4,option5 FROM questions WHERE id=".$row['questionNum']));
@@ -92,29 +125,29 @@
                         else if($row['correct'] == 2) $correct = "<span class='red'>unanswered</span>";
                         else $correct = "<span class='red'>incorrect</span>";
                         echo "<h3>Question ".$i.": ".$correct."</h3>
-                            <div class='answers-item'><b>".$questions['question']."</b><br><input type='radio' disabled ";
+                            <div class='answers-item'><b>".$questions['question']."</b><br><label><input type='radio' disabled ";
                         if($row['option'] == 1) echo "checked";
-                            echo "/>".$questions['option1']."<br><input type='radio' disabled ";
+                            echo "/>".$questions['option1']."</label><br><label><input type='radio' disabled ";
                         if($row['option'] == 2) echo "checked";
-                            echo "/>".$questions['option2']."<br>";
+                            echo "/>".$questions['option2']."</label><br>";
                             if($questions['option3']) {
-                                echo "<input type='radio' disabled ";
+                                echo "<label><input type='radio' disabled ";
                                 if($row['option'] == 3) echo "checked";
-                                echo "/>".$questions['option3']."<br>";
+                                echo "/>".$questions['option3']."</label><br>";
                             }
                             if($questions['option4']) {
-                                echo "<input type='radio' disabled ";
+                                echo "<label><input type='radio' disabled ";
                                 if($row['option'] == 4) echo "checked";
-                                echo "/>".$questions['option4']."<br>";
+                                echo "/>".$questions['option4']."</label><br>";
                             }
                             if($questions['option5']) {
-                                echo "<input type='radio' disabled ";
+                                echo "<label><input type='radio' disabled ";
                                 if($row['option'] == 5) echo "checked";
-                                echo "/>".$questions['option5']."<br>";
+                                echo "/>".$questions['option5']."</label><br>";
                             }
                         echo "</div>";
                     }
-                    echo "</div>";
+                    echo "</div><br><br><input type='button' class='warning' onclick='deleteAttempt();' name='delete' value='Delete Attempt' />";
                 }
             }
         }
