cd94452310ba15fbdfe4a7715d86bee722c507dc
Drew-Killeen <drewkilleen00@gmail.com>
2017-01-19 19:42:38 -0500

    formatting update

--- grade.php
+++ grade.php
@@ -1,8 +1,4 @@
-<?php
-
-require 'scripts/header.php'; 
-
-?>
+<?php require 'scripts/header.php'; ?>
 
 <!DOCTYPE html>
 <html>
@@ -70,13 +66,15 @@ require 'scripts/header.php';
             }
         }
         else {
-            $score = mysqli_fetch_assoc(mysqli_query ($link, "SELECT usr,score FROM attempts WHERE id=".$_GET['attempt']));
+            $score = mysqli_fetch_assoc(mysqli_query ($link, "SELECT examNum,usr,score FROM attempts WHERE id=".$_GET['attempt']));
             if($_SESSION['id'] != $score['usr'] && ($_SESSION['privilege'] != 'admin' && $_SESSION['privilege'] != 'sysop')) {
                 echo "You do not have permission to view this attempt.";
             }
             else {
                 $answers = mysqli_query($link, "SELECT questionNum,correct,option FROM answers WHERE attemptNum=".$_GET['attempt']);
-                echo "<h4>Score: ".$score['score']."%</h4>";
+                $correctNum = mysqli_query($link, "SELECT id FROM answers WHERE attemptNum=".$_GET['attempt']." AND correct=1");
+                $examInfo = mysqli_fetch_assoc(mysqli_query($link, "SELECT module,title FROM exams WHERE id=".$score['examNum']));
+                echo "<h1>Attempt for Module ".$examInfo['module'].": ".$examInfo['title']."</h1><span class='attempt-score'><b>Score:</b> ".mysqli_num_rows($correctNum)." out of ".mysqli_num_rows($answers)." - ".$score['score']."%</span><span class='attempt-time'>--time--</span><hr>";
                 echo "<div class='answers-list'>";
                 for($i = 1; $row = mysqli_fetch_assoc($answers); $i++) {
                     $questions = mysqli_fetch_assoc(mysqli_query($link, "SELECT question,option1,option2,option3,option4,option5 FROM questions WHERE id=".$row['questionNum']));
@@ -84,25 +82,25 @@ require 'scripts/header.php';
                     else if($row['correct'] == 2) $correct = "<span class='red'>unanswered</span>";
                     else $correct = "<span class='red'>incorrect</span>";
                     echo "<h3>Question ".$i.": ".$correct."</h3>
-                        <div class='answers-item'><b>".$questions['question']."</b><br><input type='radio' disabled ";
+                        <div class='answers-item'><b>".$questions['question']."</b><br><label><input type='radio' disabled ";
                     if($row['option'] == 1) echo "checked";
-                        echo "/>".$questions['option1']."<br><input type='radio' disabled ";
+                        echo "/>".$questions['option1']."</label><br><label><input type='radio' disabled ";
                     if($row['option'] == 2) echo "checked";
-                        echo "/>".$questions['option2']."<br>";
+                        echo "/>".$questions['option2']."</label><br>";
                         if($questions['option3']) {
-                            echo "<input type='radio' disabled ";
+                            echo "<label><input type='radio' disabled ";
                             if($row['option'] == 3) echo "checked";
-                            echo "/>".$questions['option3']."<br>";
+                            echo "/>".$questions['option3']."</label><br>";
                         }
                         if($questions['option4']) {
-                            echo "<input type='radio' disabled ";
+                            echo "<label><input type='radio' disabled ";
                             if($row['option'] == 4) echo "checked";
-                            echo "/>".$questions['option4']."<br>";
+                            echo "/>".$questions['option4']."</label><br>";
                         }
                         if($questions['option5']) {
-                            echo "<input type='radio' disabled ";
+                            echo "<label><input type='radio' disabled ";
                             if($row['option'] == 5) echo "checked";
-                            echo "/>".$questions['option5']."<br>";
+                            echo "/>".$questions['option5']."</label><br>";
                         }
                     echo "</div>";
                 }
