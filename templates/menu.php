cd2d1fd6ac2b4d3933d92e44ebb8aee1d936b760
Drew-Killeen <drewkilleen00@gmail.com>
2017-02-09 12:18:44 -0500

    allow anonymous users to view parts of the menu

--- templates/menu.php
+++ templates/menu.php
@@ -1,4 +1,4 @@
-<?php if($_SESSION['id']) {
+<?php
     echo 
     '<header> 
         <span class="toggle-button">
@@ -8,29 +8,32 @@
         </span>
     <div class="menu-wrap">
         <div class="menu-sidebar">
-            <ul class="menu nolist">
-                <li><a href="../enroll/"><img src="../images/pencil-64.png" alt="Enroll" class="menu-icon"></a> <a href="../enroll" class="line">Enroll</a></li>
-                <li><a href="../course/"><img src="../images/notebook-56.png" alt="Course" class="menu-icon"></a> <a href="../course" class="line">Course</a></li>
-                <li><a href="../grade/"><img src="../images/checklist-56.png" alt="Progress" class="menu-icon"></a> <a href="../grade" class="line">Progress</a></li>';
-                if($_SESSION['privilege'] == 'teacher') {
-                    echo '<li><a href="../students/"><img src="../images/student-64.png" alt="Students" class="menu-icon"></a> <a href="../students" class="line">Students</a></li>';
+            <ul class="menu nolist">';
+                if($_SESSION['id']) {
+                    echo '<li><a href="../enroll/"><img src="../images/pencil-64.png" alt="Enroll" class="menu-icon"></a> <a href="../enroll" class="line">Enroll</a></li>';
                 }
-                if($_SESSION['privilege'] == 'admin' || $_SESSION['privilege'] == 'sysop') {
-                echo '<li><a href="../students/"><img src="../images/student-64.png" alt="Students" class="menu-icon"></a> <a href="../students" class="line">Students</a></li>
-                    <li class="menu-item-has-children"><a href="../admin/"><img src="../images/gear-64.png" alt="Admin" class="menu-icon"></a> <a href="../admin/index" class="line">Admin</a> <span class="sidebar-menu-arrow"></span>
-                    <ul class="sub-menu nolist" style="line-height:1.2;">
-                        <li><a href="../admin/edit_course" class="line">Courses</a></li>
-                        <li><a href="../admin/edit_exam" class="line">Exams</a></li>
-                        <li><a href="../admin/users" class="line">Manage users</a></li>
-                        <li><a href="../admin/mail" class="line">Send mail</a></li>
-                    </ul>
-                </li>';
+                echo '<li><a href="../course/"><img src="../images/notebook-56.png" alt="Course" class="menu-icon"></a> <a href="../course" class="line">Course</a></li>';
+                if($_SESSION['id']) {
+                    echo '<li><a href="../grade/"><img src="../images/checklist-56.png" alt="Progress" class="menu-icon"></a> <a href="../grade" class="line">Progress</a></li>';
+                    if($_SESSION['privilege'] == 'teacher') {
+                        echo '<li><a href="../students/"><img src="../images/student-64.png" alt="Students" class="menu-icon"></a> <a href="../students" class="line">Students</a></li>';
+                    }
+                    if($_SESSION['privilege'] == 'admin' || $_SESSION['privilege'] == 'sysop') {
+                    echo '<li><a href="../students/"><img src="../images/student-64.png" alt="Students" class="menu-icon"></a> <a href="../students" class="line">Students</a></li>
+                        <li class="menu-item-has-children"><a href="../admin/"><img src="../images/gear-64.png" alt="Admin" class="menu-icon"></a> <a href="../admin/index" class="line">Admin</a> <span class="sidebar-menu-arrow"></span>
+                        <ul class="sub-menu nolist" style="line-height:1.2;">
+                            <li><a href="../admin/edit_course" class="line">Courses</a></li>
+                            <li><a href="../admin/edit_exam" class="line">Exams</a></li>
+                            <li><a href="../admin/users" class="line">Manage users</a></li>
+                            <li><a href="../admin/mail" class="line">Send mail</a></li>
+                        </ul>
+                    </li>';
+                    }
                 }
                 echo 
-                '<li><a href="../about"><img src="../images/info-56.png" alt="View course" class="menu-icon"></a> <a href="../about" class="line">About</a></li>
+                '<li><a href="../about"><img src="../images/info-56.png" alt="About" class="menu-icon"></a> <a href="../about" class="line">About</a></li>
                 </ul>
             </div>
         </div>
     </header>';
-}
 ?>
